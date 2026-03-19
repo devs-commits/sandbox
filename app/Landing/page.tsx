911af903b88d2cdaa8ad1cd5746447c6ce36b78e
Lloydgapton <lloydgapton123@gmail.com>
2026-03-19 00:19:55 +0000

    added the Experience Trap problem feature to the landing page component

--- app/Landing/page.tsx
+++ app/Landing/page.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/landing/HeroSection";
+import ProblemSection from "../components/landing/ProblemSection";
 import CompanyLogoWall from "../components/landing/CompanyLogoWall";
 import CareerPathQuiz from "../components/landing/CareerPathQuiz";
 import ROICalculator from "../components/landing/ROICalculator";
@@ -11,6 +12,7 @@ const Landing = () => {
  <div className="min-h-screen bg-[hsla(207,36%,95%,1)]">
   <Navbar />
   <HeroSection />
+  <ProblemSection />
   <CompanyLogoWall />
   <CareerPathQuiz />
   <ROICalculator />
