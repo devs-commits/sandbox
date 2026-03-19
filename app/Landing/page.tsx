d4641b4186bbb40ccd2127fc1383a0a058878148
Lloydgapton <lloydgapton123@gmail.com>
2026-03-19 00:01:15 +0000

    added the marquee animation to the global.css file and add the Company Logo wall component to the landing page

--- app/Landing/page.tsx
+++ app/Landing/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import HeroSection from "../components/landing/HeroSection";
+import CompanyLogoWall from "../components/landing/CompanyLogoWall";
 
 const Landing = () => {
   return (
  <div className="min-h-screen bg-[hsla(207,36%,95%,1)]">
   <Navbar />
   <HeroSection />
+  <CompanyLogoWall />
  </div>
   )
 };
